487eaa7cd4b357242c63471af4dddfcb96631cc2
Tranducthang28011996 <tieuhodo96@gmail.com>
2017-08-02 09:58:37 +0700

    add Twitter Bootstrap
    conflict gemlock
    
    b

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/new'
+
+  root 'static_pages#home'
+  get 'home' => 'static_pages#home'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
 end
