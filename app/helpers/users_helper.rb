5847f8a351852333c7b972e4759527cac8e61e15
Tranducthang28011996 <tieuhodo96@gmail.com>
2017-08-02 22:50:28 +0700

    chapter-7

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+  def gravatar_for(user, size: 80)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+
+  def custom_erros(user)
+      user.errors.messages.each do |msg| 
+      @name_error = msg[1].last if msg[0].eql?(:name)
+      @email_error = msg[1].last if msg[0].eql?(:email)
+      @password_error = msg[1].last if msg[0].eql?(:password)
+      @password_confir_error = msg[1].last if msg[0].eql?(:password_confirmation)
+    end if user.errors.any?
+  end
 end
